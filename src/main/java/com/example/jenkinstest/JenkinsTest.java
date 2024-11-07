6c7423888c58ececbdee2c6bbfd324c374e22931
kkm4512 <nayoun440@naver.com>
2024-11-07 19:18:54 +0900

    init

---
+++ src/main/java/com/example/jenkinstest/JenkinsTest.java
@@ -0,0 +1,12 @@
+package com.example.jenkinstest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsTest {
+    @GetMapping
+    public String test1(){
+        return "Hello World";
+    }
+}
