e01c363fefc6c5685c660aa2fd200926f0d46c44
kkm4512 <nayoun440@naver.com>
2024-11-11 14:42:45 +0900

    CI-TEST 14

--- src/main/java/com/example/jenkinstest/JenkinsTest.java
+++ src/main/java/com/example/jenkinstest/JenkinsTest.java
@@ -1,26 +1,17 @@
 package com.example.jenkinstest;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-// TEST
 
 public class JenkinsTest {
-    private final RedisTemplate<String, String> redisTemplate;
 
     @GetMapping("/test")
     public String test(){
         return "Hello World";
-    }
-
-    @GetMapping("/test1")
-    public String test1(){
-        redisTemplate.opsForValue().set("hello", "world");
-        return redisTemplate.opsForValue().get("hello");
     }
 
 
